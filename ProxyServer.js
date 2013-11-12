d25c15ccd29ef1c23e31c3dcae9a78256b09a478
Sebastian Patten <sebastianpatten@gmail.com>
2013-11-11 20:47:01 -0700

    Re-jigged to create methods via prototype

--- ProxyServer.js
+++ ProxyServer.js
@@ -1,16 +1,21 @@
 "use strict";
 
 var winston = require('winston');
+var http = require('http');
 
-module.exports = function ProxyServer(port) {
-    var running = false;
-
-    this.start = function() {
-        winston.info("Proxy server started on " + port);
-
-//        http.createServer(function (request, response) {
-////    winston.info("Received request for: " + request.url);
-////    forwardRequestAndProcessResponse(request, response, new TransparentProxy().process);
-//        }).listen(proxyServerPort);
-    };
+var ProxyServer = function(port) {
+    var self = this;
+    self.port = port;
 };
+
+ProxyServer.prototype.start = function() {
+    var self = this;
+    winston.info("Proxy server started on " + self.port);
+
+//    http.createServer(function (request, response) {
+//        winston.info("Received request for: " + request.url);
+//    //    forwardRequestAndProcessResponse(request, response, new TransparentProxy().process);
+//    }).listen(proxyServerPort);
+};
+
+module.exports = ProxyServer;
