c981ca52153997dedfaa08ddbf5b4f8ba62150e3
Sebastian Patten <sebastianpatten@gmail.com>
2013-11-12 22:26:17 -0700

    Added underscore. Also transparently return the original headers

--- Strategies/Headers/ReturnOriginalHeaders.js
+++ Strategies/Headers/ReturnOriginalHeaders.js
@@ -2,7 +2,7 @@ var winston = require('winston');
 
 module.exports.process = function(realResponse, modifiedResponse) {
     winston.info("Transparently setting headers. Details: " + JSON.stringify(realResponse.headers));
-    modifiedResponse.writeHead(realResponse.statusCode, {'Content-Type': 'text/plain'});
+    modifiedResponse.writeHead(realResponse.statusCode, realResponse.headers);
     modifiedResponse.end();
 };
 
