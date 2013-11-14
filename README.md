37aea565b1634da7382acf31403bbda8f6ed83b9
Sebastian Patten <sebastianpatten@gmail.com>
2013-11-13 20:58:22 -0700

    Added mocha and chai

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-fiddle
+Fiddle
 ======
 
-A Node.js web debugging proxy server
+Fiddle is a Node.js web debugging proxy server.
+
+If you don't specify any conditions it will act as a transparent proxy server and not
--- fiddle.js
+++ fiddle.js
@@ -3,15 +3,7 @@
 var http = require('http');
 var winston = require('winston');
 var cfg = require('./Config');
-var ProxyServer = require('./ProxyServer');
+var ProxyServer = require('./lib/ProxyServer');
 
 var proxyServer = new ProxyServer(cfg.proxyServer.port);
 proxyServer.start();
-
-
-//function forwardRequestAndProcessResponse(request, response, delegate) {
-//    var forwardRequest = http.request(request.url, function(forwardResponse) {
-//        delegate(response, forwardResponse);
-//    });
-//    forwardRequest.end();
-//}
